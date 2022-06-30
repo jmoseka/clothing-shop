60c9c583bc1cfcf0a0b294d1f08d84bb78c79adb
jmoseka <jamylam3@gmail.com>
2022-06-30 13:04:36 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/routes/home/home.component';
+import Navigation from './components/routes/navigation/navigation.component';
+
+const Shop = () => <h1>I am the shop page</h1>;
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Navigation />}>
+
+      <Route index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+
+    </Route>
+
   </Routes>
 );
 
