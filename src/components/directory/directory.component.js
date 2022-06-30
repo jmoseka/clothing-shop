e857e8cbedd66e173ff64a6ee3df552c2d34c18b
jmoseka <jamylam3@gmail.com>
2022-06-30 10:48:13 +0300

    Add react router

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CategoryItem from '../category-item/category-item-component';
+import './directory.styles.scss';
 
 function Directory(props) {
   const p = props;
